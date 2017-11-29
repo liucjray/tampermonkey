9fbae7c2ac0328ad78d0ce4434d23377ce426fb6
LiuCJ-Ray <ray@landteam.net>
2017-11-29 11:46:55 +0800

    test

---
+++ test.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         test
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://oaoa.tech/index.php?m=&c=Flow&a=read&id=*
+// @grant        none
+// ==/UserScript==
+
+$(function(){
+    console.log(123);
+});
